52c2ec81e7c6d446fbb9fc8d72557bca257456b3
Muflone <muflone@vbsimple.net>
2013-05-01 15:55:29 +0200

    First code commit

---
+++ remmina-plugin-url/src/plugin_config.h
@@ -0,0 +1,8 @@
+#ifndef __PLUGIN_CONFIG_H
+#define __PLUGIN_CONFIG_H
+
+#define PLUGIN_NAME        "URL"
+#define PLUGIN_DESCRIPTION "URL - Open an URL in an external browser"
+#define PLUGIN_VERSION     "1.0.0.0"
+#define PLUGIN_APPICON     "remmina-url"
+#endif
