35cacb9686014d50dcccfe57f35ae8ecfd70e0a1
Muflone <webreg@vbsimple.net>
2014-11-02 15:20:03 +0100

    Updated for GTK+ 3

--- remmina-plugin-url/src/plugin_config.h
+++ remmina-plugin-url/src/plugin_config.h
@@ -1,8 +1,29 @@
-#ifndef __PLUGIN_CONFIG_H
-#define __PLUGIN_CONFIG_H
+/*
+ *     Project: Remmina Plugin URL
+ * Description: Remmina protocol plugin to open an URL in an external browser.
+ *      Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+ *   Copyright: 2011-2014 Fabio Castelli (Muflone)
+ *     License: GPL-2+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of ERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define PLUGIN_NAME        "URL"
-#define PLUGIN_DESCRIPTION "URL - Open an URL in an external browser"
-#define PLUGIN_VERSION     "1.0.0.1"
-#define PLUGIN_APPICON     "remmina-url"
+#ifndef __PLUGIN_CONFIG_H
+  #define __PLUGIN_CONFIG_H
+
+  #define PLUGIN_NAME        "URL"
+  #define PLUGIN_DESCRIPTION "URL - Open an URL in an external browser"
+  #define PLUGIN_VERSION     "1.1.1.0"
+  #define PLUGIN_APPICON     "remmina-url"
 #endif
